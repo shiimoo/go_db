6f044fcc048f562b9d0bc32086529d5a4f9465d0
shimo <1336313625@qq.com>
2024-08-19 19:12:10 +0800

    1

--- network/link.go
+++ network/link.go
@@ -1,9 +1,15 @@
 package network
 
+import "github.com/shiimoo/godb/lib/base/service"
+
 type Link interface {
+	service.Service
 	Key() any              // 标识格式待定
-	Start() error          // 开启监听
-	Close() error          // 关闭链接
 	Read() ([]byte, error) // 读取数据
 	Write([]byte) error    // 写入数据
 }
+
+// 消息体字节长度([4]byte)
+// 包体总数([2]byte)
+// 当前包体序号([2]byte)
+// 包体节流([65535]byte)
--- network/tcp/link.go
+++ network/tcp/link.go
@@ -25,6 +25,43 @@ func NewLink(parent context.Context, baseLink *net.TCPConn, id uint) *TcpLink {
 	return link
 }
 
+// func (l *TcpLink) Start() error {
+// 	panic(" Service Sub Class need to realize Service interface func Start() error")
+// }
+
+// func (l *TcpLink) Stop() error {
+// 	panic(" Service Sub Class need to realize Service interface func Stop() error")
+// }
+
+// func (l *TcpLink) Close() error {
+// 	panic(" Service Sub Class need to realize Service interface func Close() error")
+// }
+
 func (l *TcpLink) Key() uint {
 	return l.id
 }
+
+func (l *TcpLink) Read() ([]byte, error) {
+
+	// 该读取会读取一个完整的消息体
+
+	// 包体总数([2]byte)
+	// 当前包体序号([2]byte)
+	// 包体字节总长度([2]byte)
+	// 包体字节流([65535]byte)
+
+	// bs := make([]byte, 1024)
+	// n, err := tcpConn.Read(bs)
+	// if err != nil {
+	// 	mlog.Warn("tcp", "acceptTCP", err.Error())
+	// } else {
+	// 	bs = bs[:n]
+	// }
+	// mlog.Infof("tcp", "", "%s-%s", []mlog.Data{
+	// 	{Key: "data", Value: string(bs)}, {Key: "n", Value: n},
+	// }...)
+}
+
+// func (l *TcpLink) Write([]byte) error {
+// 	return nil
+// }
